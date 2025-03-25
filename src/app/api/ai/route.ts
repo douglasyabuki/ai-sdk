25745b9fc7aa9dba5a5f24707b18bb207a3d2a1a
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-25 07:27:51 -0300

    add: text responses streaming / chat components

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,19 +1,51 @@
 import { openrouter } from "@/ai/open-router";
-import { generateObject } from "ai";
-import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod';
+import { streamText, tool } from "ai";
+import { NextRequest } from "next/server";
+import { z } from "zod";
 
-export async function GET(request: NextRequest) {
-  const result = await generateObject({
-    model: openrouter.chat('openai/gpt-4o-2024-11-20'),
-    schema: z.object({
-      pt: z.string().describe("Tradução para português"),
-      fr: z.string().describe("Tradução para francês"),
-      es: z.string().describe("Tradução para espanhol"),
-    }),
-    prompt: 'Traduza "Hello World" para diferentes idiomas!',
-    system: 'Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.'
-  })
+export async function POST(request: NextRequest) {
+  const { messages } = await request.json();
 
-  return NextResponse.json({ message: result.object })
+  const result = streamText({
+    model: openrouter.chat("openai/gpt-4o-2024-11-20"),
+    tools: {
+      profileAndUrls: tool({
+        description:
+          "Essa ferramenta serve para buscar do perfil de um usuário do GitHub ou acessar URLs da API para outras informações de um usuário como lista de organizações, repositórios, eventos, seguidores, seguindo, etc...",
+        parameters: z.object({
+          username: z.string().describe("Username do usuário no GitHub"),
+        }),
+        execute: async ({ username }) => {
+          const response = await fetch(
+            `https://api.github.com/users/${username}`
+          );
+          const data = await response.json();
+
+          return JSON.stringify(data);
+        },
+      }),
+
+      fetchHTTP: tool({
+        description:
+          "Essa ferramenta serve para realizar uma requisição HTTP em uma URL especificada e acessar sua resposta",
+        parameters: z.object({
+          url: z.string().describe("URL a ser requisitada"),
+        }),
+        execute: async ({ url }) => {
+          const response = await fetch(url);
+          const data = await response.text();
+
+          return data;
+        },
+      }),
+    },
+    messages,
+    maxSteps: 5,
+    system: `Sempre responda em markdown sem aspas no início ou final da mensagem.`,
+    onStepFinish({ toolResults }) {
+      console.log(toolResults);
+    },
+  });
+
+  return result.toDataStreamResponse();
 }
