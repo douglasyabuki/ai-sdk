cc4597ee497607e777afe90376e666b23678b792
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-03-25 06:08:28 -0300

    add: openai integration

---
+++ src/app/api/ai/route.ts
@@ -0,0 +1,14 @@
+import { openai } from "@ai-sdk/openai";
+import { generateText } from "ai";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const result = await generateText({
+    model: openai("gpt-4o"),
+    prompt: 'Traduza "Hello World" para português!',
+    system:
+      "Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.",
+  });
+
+  return NextResponse.json({ message: result.text });
+}
